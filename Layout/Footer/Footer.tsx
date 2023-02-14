421e3e59a260bd441fce5814e791b8aaa623253a
Mykhailo <mihasokol1993@gmail.com>
2023-02-14 02:05:37 +0200

    completed basic layout using grid

--- Layout/Footer/Footer.tsx
+++ Layout/Footer/Footer.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import { FooterProps } from '@/Layout/Footer/Footer.props'
+import cn from 'classnames'
+import styles from './Footer.module.css'
+import { format } from 'date-fns'
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>Owl top 2020 - {format(new Date(), 'yyyy')} All rights reserved</div>
+      <a href='#'>User Agreement</a>
+      <a href='#'>Policy of Confidentiality</a>
+    </footer>
+  )
 }
--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -3,17 +3,16 @@ import { LayoutProps } from '@/Layout/Layout.props'
 import { Header } from '@/Layout/Header/Header'
 import { Sidebar } from '@/Layout/Sidebar/Sidebar'
 import { Footer } from '@/Layout/Footer/Footer'
+import styles from './Layout.module.css'
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        <div>{children}</div>
-      </div>
-      <Footer />
-    </>
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>{children}</div>
+      <Footer className={styles.footer} />
+    </div>
   )
 }
 
